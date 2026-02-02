rcar-community/meta-sparrow-hawk

commit 82897cb0161a5621fb8a6f893eaf247abd5ee62b

build.sh: Fix SDK build target
Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

build.sh
@@ -143,7 +143,10 @@ fi
 
 bitbake ${TARGET_IMAGE}
 if [[ "${IS_BUILD_SDK}" == "yes" ]]; then
-    bitbake ${TARGET_IMAGE} -c populate_sdk
+    if [[ "${TARGET_IMAGE}" == "core-image-weston" ]];then
+        TARGET_IMAGE=${TARGET_IMAGE}-sdk
+    fi
+    bitbake ${TARGET_IMAGE}-sdk -c populate_sdk
 fi
 
 # Cleanup symbolic link

commit 199769cec47c28e48ec106563644eb43fed9eb2f

build.sh: Fix build error when building SDK
Fixes: 82897cb0161a ("build.sh: Fix SDK build target")

Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

build.sh
@@ -146,7 +146,7 @@ if [[ "${IS_BUILD_SDK}" == "yes" ]]; then
     if [[ "${TARGET_IMAGE}" == "core-image-weston" ]];then
         TARGET_IMAGE=${TARGET_IMAGE}-sdk
     fi
-    bitbake ${TARGET_IMAGE}-sdk -c populate_sdk
+    bitbake ${TARGET_IMAGE} -c populate_sdk
 fi
 
 # Cleanup symbolic link