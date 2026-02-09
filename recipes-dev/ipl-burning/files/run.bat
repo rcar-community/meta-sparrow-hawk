d834e942f4fbc625022fc96db639c8c541a983ec
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2026-02-09 17:10:17 +0900

    ipl-burning: Add copyright and SPDX License identifier
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- recipes-dev/ipl-burning/files/run.bat
+++ recipes-dev/ipl-burning/files/run.bat
@@ -1,4 +1,8 @@
 @echo off
+REM
+REM Copyright (c) 2025-2026, Renesas Electronics Corporation. All rights reserved.
+REM SPDX-License-Identifier: MIT
+REM
 
 set PATH=%PATH%;%~dp0\python-embed-amd64\Scripts
 
--- recipes-dev/ipl-burning/files/run.sh
+++ recipes-dev/ipl-burning/files/run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-
+#
+# Copyright (c) 2025-2026, Renesas Electronics Corporation. All rights reserved.
+# SPDX-License-Identifier: MIT
+#
 SCRIPT_DIR=$(cd `dirname $0` && pwd)
 
 cd ${SCRIPT_DIR}
